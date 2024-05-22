8f7bb4ae3eba991f2ee7651101a53082b8eeb1af
Talha Anwar <mtalhaanwar359@gmail.com>
2024-05-22 22:08:12 +0500

    Pointers update

--- Pointers/Pointers.cpp
+++ Pointers/Pointers.cpp
@@ -6,4 +6,10 @@ int main() {
     int *ptr = &a; //Know ptr stores the address of the variable a
     cout << "Value of a: " << a << endl;
     cout << "Address of a: " << ptr << endl;
+    int &b = a; //Know b is a reference to a
+    cout << "Value of b: " << b << endl;
+    b = 20;
+    cout << "value of a is know change using reference: " << a << endl;
+    cout<< "Address of a and b is same: " << ptr << " " << &b << endl; //Both will have same address
+    return 0;
 }
